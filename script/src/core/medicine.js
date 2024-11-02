387e0067fc5b7f05b503c88b3b7cdf71047c6024
Jarlyyn <jarlyyn@qq.com>
2024-11-03 01:45:07 +0800

    fix

--- script/src/core/medicine.js
+++ script/src/core/medicine.js
@@ -66,7 +66,7 @@
         if (App.Data.Player.HP["精气百分比"] <= 20) {
             return eatyao("疗精")
         }
-        if (App.Core.Dispel.Need && (App.Data.Player.HP["当前精力"] * 100 / App.Data.Player.HP["精力上限"]) <= 10) {
+        if (App.Core.Dispel.Need && (App.Data.Player.HP["当前内力"] * 100 / App.Data.Player.HP["内力上限"]) <= 10) {
             return eatyao("回内")
         }
         return null
